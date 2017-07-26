d46b18187ba7723719abac38a3fd719c93a19a4f
chenlc <243461254@qq.com>
2017-07-26 19:06:17 +0800

    新增Home

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Route, Link } from 'react-router-dom'
 
-import Header from '../components/Header'
+import Header from '../components/common/Header'
 import Login from '../components/Login'
 import Register from '../components/Register'
 
